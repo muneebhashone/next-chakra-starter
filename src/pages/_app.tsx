8b136f06d03e3bd13fd3e99de8f1e77084727e33
muneebhashone <muneeb.hashone@gmail.com>
2023-04-04 14:26:50 +0500

    Improvements to Chakra theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Montserrat } from "next/font/google";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import chakraTheme from "@/theme";
@@ -15,14 +14,10 @@ const queryClient = new QueryClient({
   },
 });
 
-const montserrat = Montserrat({ subsets: ["latin"] });
-
-const theme = chakraTheme(montserrat.style.fontFamily);
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={chakraTheme}>
         <NextNProgress />
         <Component {...pageProps} />
       </ChakraProvider>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,30 +2,32 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 import type { StyleFunctionProps } from "@chakra-ui/styled-system";
+import { Montserrat } from "next/font/google";
 
-const chakraTheme = (fontFamily: string) =>
-  extendTheme({
-    colors: {
-      brand: {
-        900: "#1a365d",
-        800: "#153e75",
-        700: "#2a69ac",
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+const chakraTheme = extendTheme({
+  colors: {
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
+    },
+  },
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        fontFamily: montserrat.style.fontFamily,
+        color: mode("gray.800", "whiteAlpha.900")(props),
+        bg: mode("white", "gray.800")(props),
+        lineHeight: "base",
       },
-    },
-    styles: {
-      global: (props: StyleFunctionProps) => ({
-        body: {
-          fontFamily: fontFamily,
-          color: mode("gray.800", "whiteAlpha.900")(props),
-          bg: mode("white", "gray.800")(props),
-          lineHeight: "base",
-        },
-      }),
-    },
-    config: {
-      initialColorMode: "light",
-      useSystemColorMode: false,
-    },
-  });
+    }),
+  },
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+});
 
 export default chakraTheme;
